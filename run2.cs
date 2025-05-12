c5609bf11fa2000a09613049e4350e03d397f5e9
ILYA <ylmaskylov-ilia@mail.ru>
2025-05-12 21:41:57 +0500

    анлак

---
+++ run2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+class Program
+{
+    // Константы для символов ключей и дверей
+    static readonly char[] keys_char = Enumerable.Range('a', 26).Select(i => (char)i).ToArray();
+    static readonly char[] doors_char = keys_char.Select(char.ToUpper).ToArray();
+    
+    // Метод для чтения входных данных
+    static List<List<char>> GetInput()
+    {
+        var data = new List<List<char>>();
+        string line;
+        while ((line = Console.ReadLine()) != null && line != "")
+        {
+            data.Add(line.ToCharArray().ToList());
+        }
+        return data;
+    }
+
+
+    static int Solve(List<List<char>> data)
+    {
+        return 52;
+    }
+    
+    static void Main()
+    {
+        var data = GetInput();
+        int result = Solve(data);
+        
+        if (result == -1)
+        {
+            Console.WriteLine("No solution found");
+        }
+        else
+        {
+            Console.WriteLine(result);
+        }
+    }
+}
